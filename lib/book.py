19521194872c8ec822e488f0d5af5382cb72ae7f
Aigul <aigul.djon@gmail.com>
2023-10-09 17:08:00 +0100

    Improved an issue in book.py

--- lib/book.py
+++ lib/book.py
@@ -4,6 +4,14 @@ class Book:
         self.title = title
         self.author_name = author_name
 
+    def __eq__(self, other):
+        return (
+            isinstance(other, Book) and
+            self.id == other.id and
+            self.title == other.title and
+            self.author_name == other.author_name
+        )
+
     def __repr__(self):
         return f"{self.id} - {self.title} - {self.author_name}"
     
